05a8f2315c24ade4364a2335b847e97e8dd2befe
Holyolg <63580448+Holyolg@users.noreply.github.com>
2022-09-17 12:11:00 +0300

    Delete Iphone14Progit directory

--- Iphone14Progit/menu-script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".header__menu-btn");
-const nav = document.querySelector(".header__nav-row");
-
-btn.addEventListener("click", function () {
-  nav.classList.toggle("menu-open");
-});
